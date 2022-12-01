0f5a4c211728a5f6d908365c30e8548e708c3978
sam <1581400575@qq.com>
2022-12-01 23:39:22 +0800

    rabbit配置&链接

---
+++ config.go
@@ -0,0 +1,9 @@
+package rabbit
+
+type rabbitConfig struct {
+	Host     string
+	Username string
+	Password string
+	Protocol string
+	Port     int
+}
--- connect.go
+++ connect.go
@@ -1,69 +1,33 @@
 package rabbit
 
 import (
-	"errors"
-	"io"
-	"net"
+	"github.com/farseer-go/fs/configure"
+	"github.com/streadway/amqp"
 	"strconv"
 )
 
-var errURIScheme = errors.New("AMQP scheme must be either 'amqp://' or 'amqps://'")
-var errURIWhitespace = errors.New("URI must not contain whitespace")
-
-var schemePorts = map[string]int{
-	"amqp":  5672,
-	"amqps": 5671,
-}
-
-var defaultURI = URI{
-	Scheme:   "amqp",
-	Host:     "localhost",
-	Port:     5672,
-	Username: "guest",
-	Password: "guest",
-	Vhost:    "/",
-}
-
-// URI represents a parsed AMQP URI string.
-type URI struct {
-	Scheme   string
-	Host     string
-	Port     int
-	Username string
-	Password string
-	Vhost    string
-}
-
 type Connect struct {
-	conn io.ReadWriteCloser
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
 }
 
 // New 创建连接
-func New(uri URI) (net.Conn, error) {
-	var con net.Conn
-	var err error
-	if uri.Scheme == "" {
-		uri.Scheme = defaultURI.Scheme
-	} else if uri.Scheme != "amqp" && uri.Scheme != "amqps" {
-		err = errors.New("network is wrong(Please select either amqps or amqp)")
-		return con, err
+func New(rabbitName string) *Connect {
+	configString := configure.GetString("Rabbit." + rabbitName)
+	if configString == "" {
+		panic("[farseer.yaml]找不到相应的配置：Rabbit." + rabbitName)
 	}
-
-	if uri.Host == "" {
-		uri.Host = defaultURI.Host
+	rabbitConfig := configure.ParseConfig[rabbitConfig](configString)
+	conn, err := amqp.Dial(rabbitConfig.Protocol + "://" + rabbitConfig.Username + ":" + rabbitConfig.Password + "@" + rabbitConfig.Host + ":" + strconv.Itoa(rabbitConfig.Port) + "")
+	if err != nil {
+		panic(err)
 	}
-	if uri.Port == 0 {
-		uri.Port = defaultURI.Port
+	var connect Connect
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
 	}
-	if uri.Username == "" {
-		uri.Username = defaultURI.Username
-	}
-	if uri.Password == "" {
-		uri.Password = defaultURI.Password
-	}
-	if uri.Vhost == "" {
-		uri.Vhost = defaultURI.Vhost
-	}
-	con, err = net.Dial("tcp", uri.Host+":"+strconv.Itoa(uri.Port))
-	return con, err
+	connect.Channel = ch
+	connect.Conn = conn
+	return &connect
 }
