77dc9a478d10a0463c51d3787437445e709f5da7
steden <1470804@qq.com>
2023-01-08 16:46:55 +0800

    增加：flog.Panic函数

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.19
 
 require (
 	github.com/farseer-go/fs v0.5.1-0.20230107135755-8805a0dcc9e2
+	github.com/rabbitmq/amqp091-go v1.5.0
 	github.com/streadway/amqp v1.0.0
 )
 
 require (
 	github.com/kr/pretty v0.3.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- rabbitProduct.go
+++ rabbitProduct.go
@@ -1,5 +1,11 @@
 package rabbit
 
+import (
+	"fmt"
+	"github.com/farseer-go/fs/flog"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 type rabbitProduct struct {
 	Server   serverConfig
 	Exchange exchangeConfig
@@ -12,5 +18,23 @@ func newProduct(server serverConfig, exchange exchangeConfig) rabbitProduct {
 	}
 }
 func (receiver rabbitProduct) Send(message string) {
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s/", receiver.Server.UserName, receiver.Server.Password, receiver.Server.Server))
+	defer conn.Close()
+	if err != nil {
+		flog.Panicf("Failed to connect to RabbitMQ %s: %s", receiver.Server.Server, err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		flog.Panicf("Failed to open a channel %s: %s", receiver.Server.Server, err)
+	}
+	defer ch.Close()
 
+	_, _ = ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
 }
