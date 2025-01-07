21a4e594e732cfaf6d55e6292443879712e8813e
steden <1470804@qq.com>
2025-01-07 18:56:22 +0800

    format

--- eventArgs.go
+++ eventArgs.go
@@ -15,7 +15,7 @@ type EventArgs struct {
 	ContentEncoding string         // MIME content encoding
 	DeliveryMode    uint8          // queue implementation use - non-persistent (1) or persistent (2)
 	Priority        uint8          // queue implementation use - 0 to 9
-	CorrelationId   string         // application use - correlation identifier
+	CorrelationId   string         // traceId
 	ReplyTo         string         // application use - address to reply to (ex: RPC)
 	Expiration      string         // implementation use - message expiration spec
 	MessageId       string         // application use - message identifier
