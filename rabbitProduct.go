ce38f1f8d7281313c96cfd1616a880ce32e26f00
steden <1470804@qq.com>
2025-03-13 16:57:57 +0800

    移除测试

--- rabbitProduct.go
+++ rabbitProduct.go
@@ -8,7 +8,6 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/farseer-go/fs/container"
 	"github.com/farseer-go/fs/core"
 	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/parse"
@@ -142,12 +141,8 @@ func (receiver *rabbitProduct) SendStringKey(message, routingKey string) error {
 
 // SendJsonKey 发送消息（使用配置设置）
 func (receiver *rabbitProduct) SendJsonKey(data any, routingKey string) error {
-	traceTest := container.Resolve[trace.IManager]().TraceHand("snc.Marshal")
 	message, _ := snc.Marshal(data)
-	traceTest.End(nil)
-	traceTest = container.Resolve[trace.IManager]().TraceHand("md5.Sum")
 	messageId := fmt.Sprintf("%x", md5.Sum(message))
-	traceTest.End(nil)
 	return receiver.SendMessage(message, routingKey, messageId, 0)
 }
 
